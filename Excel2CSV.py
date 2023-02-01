426819345ecbecebe111a98ef4dc3e77f083d7f5
Shimizu Tetsuya <shimizu@info.shonan-it.ac.jp>
2023-02-01 14:28:27 +0900

    Update Excel2CSV.py

--- Excel2CSV.py
+++ Excel2CSV.py
@@ -1,7 +1,14 @@
 import tkinter
 
+# Window
 window = tkinter.Tk()
 window.title("Excel2CSV")
 window.geometry("600x600")
+
+# Label
+label = tkinter.Label(text='label pack')
+label.pack()
+label_2 = tkinter.Label(text='Label_2 place')
+label_2.place(x=200, y=200)
 
 window.mainloop()
